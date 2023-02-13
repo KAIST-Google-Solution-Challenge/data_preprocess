0e932cf67e90a9a758f0082932a86cb4310a6d18
Jongeun Park <abepje@kaist.ac.kr>
2023-02-13 09:41:07 +0900

    initial commit. audio_url_collect.py는 audio 파일을 담고 있는 페이지의 url을 수집, audio_datae_collect.py는 각 페이지에서 audio 파일 의 url을 수집, audio_download.py는 audio 파일의 url을 통해 audio 파일을 다운로드. video 관련파일들에 대해서도 동일.

---
+++ audio_data_collect.py
@@ -0,0 +1,124 @@
+from bs4 import BeautifulSoup
+import re
+import time
+import logging
+from colorlog import ColoredFormatter
+import pdb
+import requests
+import pandas as pd
+
+# 로그 생성
+lg = logging.getLogger()
+lg.handlers = []       # No duplicated handlers
+lg.propagate = False   # workaround for duplicated logs in ipython
+
+# 로그의 출력 기준 설정
+lg.setLevel(logging.DEBUG)
+
+# log 출력 형식
+# formatter = logging.Formatter('\033[92m[%(asctime)s] - [%(name)s] - [%(levelname)s] - %(message)s\033[0m')
+formatter = ColoredFormatter(
+    # "%(log_color)s[%(asctime)s] %(message)s",
+    '%(log_color)s [%(asctime)s] - [%(name)s] - [%(levelname)s] - %(message)s',
+    datefmt=None,
+    reset=True,
+    log_colors={
+        'DEBUG':    'blue',
+        'INFO':     'white,bold',
+        'INFOV':    'cyan,bold',
+        'WARNING':  'yellow',
+        'ERROR':    'red,bold',
+        'CRITICAL': 'red,bg_white',
+    },
+    secondary_log_colors={},
+    style='%'
+)
+# log 출력
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+lg.addHandler(stream_handler)
+
+# import urllib.request
+# ...
+# video_url = video.get_property('src')
+# urllib.request.urlretrieve(video_url, 'videoname.mp4')
+
+
+def getSource(page_source: str):
+
+    # pdb.set_trace()
+    html = page_source
+    soup = BeautifulSoup(html, "lxml")
+    # soup = BeautifulSoup(r.content,"html.parser")
+    
+    data = list()
+
+    try:
+        # content = soup.select('.bd-list tbody')[0].text
+        sourcePocket = soup.select('#content .bd-view .dbdata')
+        videoSource = sourcePocket[0].select_one('video')['src']
+        lg.warning(videoSource)
+        data.append('https://www.fss.or.kr/'+videoSource)
+        # pdb.set_trace()
+        scriptSource = sourcePocket[0].select('div p')
+        if len(scriptSource) == 0 or (len(scriptSource) == 1 and scriptSource[0].text == ''):
+            lg.error('No Script Source')
+            data.append("")
+        else:
+            # lg.warning(scriptSource[0].text)
+            for p in scriptSource:
+                if not p.text.isspace():
+                    data.append(p.text)
+            
+        
+    except Exception as e:
+        lg.error(f"error: {e}")
+        
+    return data
+
+# def move_next(driver):
+#     right = driver.find_element_by_css_selector("div._aaqg._aaqh")
+#     right.click()
+#     time.sleep(2)
+
+# options = webdriver.ChromeOptions()
+# options.add_argument('--headless')        # Head-less 설정
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
+# # driver = webdriver.Chrome('chromedriver', options=options)
+# driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+srcUrls = pd.read_csv("voice_phishing_data_urls.csv")['url'].tolist()
+
+data = list()
+for index, url in enumerate(srcUrls):
+    response = requests.get(url)
+    lg.info(f"{index}th url get successful")
+    data.append(getSource(response.content))
+    
+    # if index == 300:
+    #     break
+    
+
+
+
+results_df = pd.DataFrame(data)
+# print(results_df)
+# results_df.columns = ['videosource_url', 'transcript']
+results_df.to_csv('videosource_and_transcript_utf8.csv', index=False)
+results_df.to_csv('videosource_and_transcript_cp949.csv', index=False, encoding='cp949')
+
+dataWithTranscripts = data.loc[pd.notna(data['1']), '1':]
+dataWithoutTranscripts = data.loc[pd.isna(data['1']), '0']
+
+dataWithTranscripts.to_csv('script_available_data.csv', index=False, encoding='utf-8')
+
+dataWithoutTranscripts.to_csv('src_for_noscripts.csv', index=False, encoding='utf-8')
+
+
+# content = getUrlList(driver)
+
+# lg.debug(content)
+
+# https://www.fss.or.kr
+
---
+++ audio_url_collect.py
@@ -0,0 +1,177 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import re
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+import logging
+from colorlog import ColoredFormatter
+import pdb
+import requests
+import pandas as pd
+
+# 로그 생성
+logger = logging.getLogger()
+logger.handlers = []       # No duplicated handlers
+logger.propagate = False   # workaround for duplicated logs in ipython
+
+# 로그의 출력 기준 설정
+logger.setLevel(logging.DEBUG)
+
+# log 출력 형식
+# formatter = logging.Formatter('\033[92m[%(asctime)s] - [%(name)s] - [%(levelname)s] - %(message)s\033[0m')
+formatter = ColoredFormatter(
+    # "%(log_color)s[%(asctime)s] %(message)s",
+    '%(log_color)s [%(asctime)s] - [%(name)s] - [%(levelname)s] - %(message)s',
+    datefmt=None,
+    reset=True,
+    log_colors={
+        'DEBUG':    'blue',
+        'INFO':     'white,bold',
+        'INFOV':    'cyan,bold',
+        'WARNING':  'yellow',
+        'ERROR':    'red,bold',
+        'CRITICAL': 'red,bg_white',
+    },
+    secondary_log_colors={},
+    style='%'
+)
+# log 출력
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+# import urllib.request
+# ...
+# video_url = video.get_property('src')
+# urllib.request.urlretrieve(video_url, 'videoname.mp4')
+
+
+
+def get_search_url(index: int):
+    # url = f"https://www.fss.or.kr/fss/bbs/B0000207/list.do?menuNo=200691&bbsId=&cl1Cd=&pageIndex={index}&sdate=&edate=&searchCnd=1&searchWrd="
+    url = f"https://www.fss.or.kr/fss/bbs/B0000206/list.do?menuNo=200690&bbsId=&cl1Cd=&pageIndex={index}&sdate=&edate=&searchCnd=1&searchWrd="
+    logger.debug(f"fetched url: {url}")
+    return url
+
+# def select_first(driver):
+#     first = driver.find_elements_by_css_selector("div._aagw")[0]
+#     first.click()
+#     time.sleep(3)
+
+def getUrlList(page_source: str):
+
+    # pdb.set_trace()
+    # print(f'\033[93m after start \033[0m')
+    html = page_source
+    # print(f'\033[93m after html page_source \033[0m')
+    # print(html[:100])
+    soup = BeautifulSoup(html, "lxml")
+    # soup = BeautifulSoup(r.content,"html.parser")
+    print(f'\033[93m after soup \033[0m')
+    
+    srcUrls = list()
+
+    try:
+        # content = soup.select('.bd-list tbody')[0].text
+        table = soup.select('.bd-list tbody tr')
+        logger.debug(f"table content: {table}")
+        for tr in table:
+            # logger.warning("1")
+            srcUrl = tr.select_one('td:nth-child(2) a')["href"]
+            srcUrls.append('https://www.fss.or.kr'+srcUrl)
+            
+            # logger.warning("2")
+            # logger.debug(f"srcUrl: {srcUrl}")
+            # logger.warning("3")
+        # print(f'\033[93m after table \033[0m')
+    except Exception as e:
+        logger.error(f"error: {e}")
+        
+    return srcUrls
+
+# def move_next(driver):
+#     right = driver.find_element_by_css_selector("div._aaqg._aaqh")
+#     right.click()
+#     time.sleep(2)
+
+# options = webdriver.ChromeOptions()
+# options.add_argument('--headless')        # Head-less 설정
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
+# # driver = webdriver.Chrome('chromedriver', options=options)
+# driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+
+srcUrls = pd.read_csv("voice_phishing_data_urls.csv")['url'].tolist()
+for index in range(1, 19):
+    response = requests.get(get_search_url(index))
+    logger.info(f"{index}th driver get successful")
+    # time.sleep(5)
+    srcUrls += getUrlList(response.content)
+
+
+
+results_df = pd.DataFrame(srcUrls)
+# print(results_df)
+results_df.columns = ['url']
+results_df.to_csv('audio_url.csv', index=False)
+
+
+# content = getUrlList(driver)
+
+# logger.debug(content)
+
+
+
+# * login mechanism for instagram login
+# email = "abepje@naver.com"
+# input_id = driver.find_elements_by_css_selector('input._aa4b._add6._ac4d')[0]
+# input_id.clear()
+# input_id.send_keys(email)
+
+# password = "qkrWhd@34E"
+# input_pw = driver.find_elements_by_css_selector('input._aa4b._add6._ac4d')[1]
+# input_pw.clear()
+# input_pw.send_keys(password)
+# input_pw.submit()
+
+# time.sleep(5)
+
+
+
+# # word = input("검색어를 입력하세요 : ")
+# word = "kingcharles"
+# word = str(word)
+# url = get_search_url(word)
+
+# driver.get(url)
+# time.sleep(10)
+
+# select_first(driver)
+
+# results = []
+
+# target_number = 10
+# for i in range(target_number):
+#     # data = getUrlList(driver)
+#     try:
+#         data = getUrlList(driver)
+#         print(f'\033[92m {data} \033[0m')
+#         results.append(data)
+#         move_next(driver)
+#     except Exception as err:
+#         time.sleep(2)
+#         print(f'\033[91m ERROR!!! {err}\033[0m')
+#         move_next(driver)
+#     time.sleep(3)
+# print(results[:2])
+
+# import pandas as pd
+# from datetime import datetime
+
+# date = datetime.today().strftime('%Y-%m-%d')
+
+# results_df = pd.DataFrame(results)
+# print(results_df)
+# results_df.columns = ['content', 'date', 'like', 'place', 'tags']
+# results_df.to_csv(date + '_about' + word + ' insta crawling.csv')
